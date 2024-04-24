88bf8888ce446ee46f7a39effa5e7becb2647876
ericndungutse <dav.ndungutse@gmail.com>
2024-04-24 16:08:52 +0200

    feat: create event model

---
+++ model/EventModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    availableTickets: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+module.exports = mongoose.model('Event', eventSchema);
