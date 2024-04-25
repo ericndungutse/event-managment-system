121181694314e0b93add95a95e97597f6b6bbbf9
ericndungutse <dav.ndungutse@gmail.com>
2024-04-25 10:21:14 +0200

    feat: create booking model

---
+++ model/bookingModel.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const bookingSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+
+    event: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Event',
+      required: true,
+    },
+
+    nummberOfTickets: {
+      type: Number,
+      required: true,
+    },
+
+    canceled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const Booking = mongoose.model('Booking', bookingSchema);
+
+export default Booking;
