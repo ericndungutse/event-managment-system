d99292ee313bf8c3aeb1f1edf353f0c8c1d617ec
ericndungutse <dav.ndungutse@gmail.com>
2024-04-27 18:04:45 +0200

    feat: add logout button on page header

--- client/src/components/HorizontalNav.jsx
+++ client/src/components/HorizontalNav.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useUser } from '../context/UserContex';
+import { CiLogin } from 'react-icons/ci';
 
 export default function HorizontalNav() {
-  const { user } = useUser();
+  const { user, signOut } = useUser();
+
+  const navigate = useNavigate();
   return (
     <nav className='flex items-center gap-4'>
       {user ? (
-        <Link
-          to='/dashboard'
-          className='text-primary-color border border-primary-color align-middle py-0.5 px-4 rounded-full font-light'
-        >
-          Dashboard
-        </Link>
+        <>
+          <Link
+            to='/dashboard'
+            className='text-white border bg-primary-color align-middle py-0.5 px-4 rounded-full font-light'
+          >
+            Dashboard
+          </Link>
+          <button
+            className=' w-full text-gray-900 font-normal hover:bg-gray-200 transition-all hover:text-white rounded p-1 flex gap-2 items-center'
+            onClick={() => {
+              signOut();
+              navigate('/');
+            }}
+          >
+            <CiLogin className='w-[1.2rem] h-[1.2rem] text-gray-500' />
+          </button>
+        </>
       ) : (
         <>
           <Link
